047499906d93ef002e5e66ed4c3a335071fab84d
andreyjlh <51566834+andreyjlh@users.noreply.github.com>
2022-01-04 19:53:35 -0600

    Add files via upload

--- Proyecto 1/main.js
+++ Proyecto 1/main.js
@@ -1,13 +1,17 @@
 
+
+
 let notecount = 0;
 let pizarracount = 0;
+
+
+
 
 function crearPizzarra() {
   const pizarras = document.getElementById("pizarras");
   const newDiv = document.createElement("div");
   newDiv.className = "pizarra";
   pizarracount++;
-
 
   //titulo,descripcion, fecha
   const color = getRandomColor();
@@ -26,8 +30,8 @@ function crearPizzarra() {
   fechaop.style.backgroundColor = color;
 
   const tiempoTranscurrido = Date.now();
-   const hoy = new Date(tiempoTranscurrido);
-  fechaop.value = hoy.toDateString();
+  const hoy = new Date(tiempoTranscurrido);
+  fechaop.value = hoy.toISOString().slice(0, 19).replace('T', ' ');
   fechaop.readOnly = true;
 
   newDiv.appendChild(titleop);
@@ -37,9 +41,10 @@ function crearPizzarra() {
 
   //categorias
 
+   //categoria iniciado
   const cat1 = document.createElement("div");
   cat1.className = "categorias";
-  cat1.ondrop =dragDrop;
+  cat1.ondrop = dragDrop;
   cat1.ondragover = allowDrop;
   //titulo
   const tituloC1 = document.createElement('input');
@@ -51,9 +56,10 @@ function crearPizzarra() {
 
   newDiv.appendChild(cat1);
 
+  //categoria iniciado
 
   const cat2 = document.createElement("div");
-  cat2.ondrop =dragDrop;
+  cat2.ondrop = dragDrop;
   cat2.ondragover = allowDrop;
 
   const tituloC2 = document.createElement('input');
@@ -65,9 +71,10 @@ function crearPizzarra() {
   cat2.className = "categorias";
   newDiv.appendChild(cat2);
 
+  //categoria finalizado
   const cat3 = document.createElement("div");
   cat3.className = "categorias";
-  cat3.ondrop =dragDrop;
+  cat3.ondrop = dragDrop;
   cat3.ondragover = allowDrop;
 
   const tituloC3 = document.createElement('input');
@@ -78,16 +85,13 @@ function crearPizzarra() {
 
   newDiv.appendChild(cat3);
 
-
-
-
   const button = document.createElement("button");
-  button.className="botonPizarra";
-  button.style.backgroundColor= color;
+  button.className = "botonPizarra";
+  button.style.backgroundColor = color;
   button.innerHTML = "X";
-  button.addEventListener("click",() =>{
+  button.addEventListener("click", () => {
     const doDelete = confirm("¿Desea eliminar esta pizarra");
-    if(doDelete){
+    if (doDelete) {
       pizarras.removeChild(newDiv);
     }
   });
@@ -95,9 +99,98 @@ function crearPizzarra() {
   // add the newly created element and its content into the DOM
 
 
-  newDiv.id = "note" + pizarracount; 
-  pizarras.appendChild(newDiv);
+  //newDiv.id = "note" + pizarracount;
 
+  pizarras.appendChild(newDiv);
+/*
+  //Insertar a la base
+
+  //Pizarra
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "pizarras.php", false);
+
+  xhttp.onreadystatechange = function () { //Call a function when the state changes.
+    if (xhttp.readyState == 4 && xhttp.status == 200) { // complete and no errors
+      alert(xhttp.responseText); // some processing here, or whatever you want to do with the response
+    }
+  };
+
+  var formData = new FormData();
+  formData.append("nombre", titleop.value);
+  formData.append("descripcion", Depop.value);
+  formData.append("fecha", fechaop.value);
+  formData.append("id_usuario", 1);
+
+  xhttp.onload = function () {
+    newDiv.id = this.responseText;
+
+  };
+
+  xhttp.send(formData);
+
+  //estados
+  //1
+  xhttp.open("POST", "estados.php", false);
+
+  xhttp.onreadystatechange = function () { //Call a function when the state changes.
+    if (xhttp.readyState == 4 && xhttp.status == 200) { // complete and no errors
+      alert(xhttp.responseText); // some processing here, or whatever you want to do with the response
+    }
+  };
+
+  var formData = new FormData();
+  formData.append("nombre", tituloC1.value);
+  formData.append("workflow_id", newDiv.id);
+  xhttp.onload = function () {
+    cat1.id = this.responseText;
+  };
+
+  xhttp.send(formData);
+
+  //2
+
+  xhttp.open("POST", "estados.php", false);
+
+  xhttp.onreadystatechange = function () { //Call a function when the state changes.
+    if (xhttp.readyState == 4 && xhttp.status == 200) { // complete and no errors
+      alert(xhttp.responseText); // some processing here, or whatever you want to do with the response
+    }
+  };
+
+  
+  var formData = new FormData();
+  formData.append("nombre", tituloC2.value);
+  formData.append("workflow_id", newDiv.id);
+
+  xhttp.onload = function () {
+    cat2.id = this.responseText;
+  };
+
+  xhttp.send(formData);
+
+//3
+
+xhttp.open("POST", "estados.php", false);
+
+xhttp.onreadystatechange = function () { //Call a function when the state changes.
+  if (xhttp.readyState == 4 && xhttp.status == 200) { // complete and no errors
+    alert(xhttp.responseText); // some processing here, or whatever you want to do with the response
+  }
+};
+
+
+var formData = new FormData();
+formData.append("nombre", tituloC3.value);
+formData.append("workflow_id", newDiv.id);
+
+xhttp.onload = function () {
+  cat3.id = this.responseText;
+};
+
+xhttp.send(formData);
+  
+*/
 }
 
 function getRandomColor() {
@@ -136,89 +229,90 @@ function crearNota() {
   note.addEventListener("dragstart", () => {
     note.classList.add('dragging');
   });
-  note.addEventListener('dragend',()=>{
+  note.addEventListener('dragend', () => {
     note.classList.remove('dragging');
+
   })
-  note.addEventListener("dblclick",() =>{
+  note.addEventListener("dblclick", () => {
     //const doDelete = confirm("¿Desea eliminar esta nota");
-      
-      //var modal = document.getElementById("myModal");
-      //var mcontent = document.getElementById("modalC");
-      //var span = document.getElementsByClassName("close")[0];
 
-      var modal = document.createElement("div");
-      modal.id = "myModal";
-      modal.className = "modal";
+    //var modal = document.getElementById("myModal");
+    //var mcontent = document.getElementById("modalC");
+    //var span = document.getElementsByClassName("close")[0];
 
-      var mcontent = document.createElement("div");
-      mcontent.id = "modalC";
-      mcontent.className = "modal-content";
+    var modal = document.createElement("div");
+    modal.id = "myModal";
+    modal.className = "modal";
 
-      var span = document.createElement("span");
-      span.className ="close";
-      span.innerHTML = "&times;";
+    var mcontent = document.createElement("div");
+    mcontent.id = "modalC";
+    mcontent.className = "modal-content";
 
-      var mfooter = document.createElement("div");
-      mfooter.className = "modal-footer";
-      
+    var span = document.createElement("span");
+    span.className = "close";
+    span.innerHTML = "&times;";
+
+    var mfooter = document.createElement("div");
+    mfooter.className = "modal-footer";
 
 
-      mcontent.appendChild(span);
-      modal.appendChild(mcontent);
-      modal.appendChild(mfooter);
-      document.body.appendChild(modal);
-      modal.style.display = "block";
 
-      
-      const eliminar = document.createElement("button");
-      eliminar.className = "notaEliminar";
-      eliminar.innerHTML = "Eliminar";
-      eliminar.onclick = function(){
-        note.remove();
-        modal.style.display = "none";
-        modal.remove();
-      }
-      mfooter.appendChild(eliminar);
+    mcontent.appendChild(span);
+    modal.appendChild(mcontent);
+    modal.appendChild(mfooter);
+    document.body.appendChild(modal);
+    modal.style.display = "block";
 
-      let colors = [ // Nine different note colors
-        'lightgoldenrodyellow',
-        'lightblue',
-        'lightgreen',
-        'lightpink',
-        'lightcoral',
-        'lightskyblue',
-        'lightsalmon',
-        'plum',
-        'lightseagreen'
+
+    const eliminar = document.createElement("button");
+    eliminar.className = "notaEliminar";
+    eliminar.innerHTML = "Eliminar";
+    eliminar.onclick = function () {
+      note.remove();
+      modal.style.display = "none";
+      modal.remove();
+    }
+    mfooter.appendChild(eliminar);
+
+    let colors = [ // Nine different note colors
+      'lightgoldenrodyellow',
+      'lightblue',
+      'lightgreen',
+      'lightpink',
+      'lightcoral',
+      'lightskyblue',
+      'lightsalmon',
+      'plum',
+      'lightseagreen'
     ];
-      colors.forEach(color => {
-        const button = document.createElement("button");
-        button.className="botonPizarra";
-        button.id = "bColor";
-        button.style.backgroundColor= color;
-        button.onclick = function(){
-          note.style.backgroundColor = color;
-        }
-        mcontent.appendChild(button);
-      });
+    colors.forEach(color => {
+      const button = document.createElement("button");
+      button.className = "botonPizarra";
+      button.id = "bColor";
+      button.style.backgroundColor = color;
+      button.onclick = function () {
+        note.style.backgroundColor = color;
+      }
+      mcontent.appendChild(button);
+    });
 
-      span.onclick = function(){
+    span.onclick = function () {
+      modal.style.display = "none";
+      modal.remove();
+    }
+    window.onclick = function (event) {
+      if (event.target == modal) {
         modal.style.display = "none";
         modal.remove();
       }
-      window.onclick = function(event) {
-        if (event.target == modal) {
-          modal.style.display = "none";
-          modal.remove();
-        }
-      }
-      //note.remove();
-    
+    }
+    //note.remove();
+
   });
   notecount++;
-  note.id = 'nota' +   notecount;
+  note.id = 'nota' + notecount;
   const notitas = document.getElementById("showNotas");
-  
+
   notitas.appendChild(note);
   //document.body.appendChild(note);
 
@@ -237,3 +331,5 @@ function getDragAfterElement(container, y) {
     }
   }, { offset: Number.NEGATIVE_INFINITY }).element
 }
+
+
